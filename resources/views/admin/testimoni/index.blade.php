3937289d5fb8c5219531862609d11c5fb630b0cd
BryanSeanAbner <bryansean425@gmail.com>
2025-07-28 16:45:31 +0700

    Update navbar links in post page to redirect to homepage sections

--- resources/views/admin/testimoni/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-@section('admin-title', 'Testimoni')
-@section('content')
-<div class="bg-white rounded shadow p-8">
-    <h2 class="text-xl font-bold mb-4">Daftar Testimoni</h2>
-    <p>Daftar testimoni akan tampil di sini.</p>
-</div>
-@endsection 
