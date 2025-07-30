d2432014d6017bc9d8d78325ebd6e682562279f8
BryanSeanAbner <bryansean425@gmail.com>
2025-07-30 10:56:08 +0700

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,16 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Bitcount+Prop+Single:wght@100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         
+        <!-- Vite Assets (Tailwind CSS) -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+        <!-- Fallback CSS untuk Railway -->
+        @if(app()->environment('production') || !app()->environment('local'))
+            <link rel="stylesheet" href="{{ asset('build/assets/app-B7JmuMcs.css') }}">
+        @endif
+        
         <!-- Custom CSS -->
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <!-- Page Content -->
