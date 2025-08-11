6d2e0ba7ccacd06980ed6b9dc1903dbf7dcff589
BryanSeanAbner <bryansean425@gmail.com>
2025-08-11 16:28:25 +0700

    Simplify UserSeeder for deployment - single admin user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,30 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // User admin yang sudah ada
+        // User admin untuk deploy dan testing
         User::firstOrCreate(
             ['username' => 'admin'],
             [
                 'password' => Hash::make('pswd'),
                 'plain_password' => 'pswd',
-            ]
-        );
-
-        // User untuk teman Anda
-        User::firstOrCreate(
-            ['username' => 'teman1'],
-            [
-                'password' => Hash::make('password123'),
-                'plain_password' => 'password123',
-            ]
-        );
-
-        // User untuk teman lainnya
-        User::firstOrCreate(
-            ['username' => 'teman2'],
-            [
-                'password' => Hash::make('password456'),
-                'plain_password' => 'password456',
             ]
         );
     }
